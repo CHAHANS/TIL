b92358ad728938ae4e5415e13b5e1811e89bcffe
CHAHANS <herodev0129@gmail.com>
2023-09-13 22:42:20 +0900

    front_playground

---
+++ frontend/playgroundtest/check_a.js
@@ -0,0 +1,15 @@
+
+var main = {
+    init : function () {
+        var _this = this;
+        $('#check-attendance-save').on('click', function (){
+            _this.check_attendance_save();
+        })
+    },
+
+    check_attendance_save : function() {
+        var data = {
+            
+        }
+    }
+}
